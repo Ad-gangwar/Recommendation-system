b871b7c16f82e6ed9d85ccde7f9737d2ff0edb52
aditya-ropo <aditya@ropohub.com>
2025-07-02 23:59:06 +0530

    made login and register page design

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
+import styles from './login.module.css';
+import Link from 'next/link';
 
-const Login = () => {
+const Login: React.FC = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className={styles.container}>
+      <h3 className={styles.heading}>
+        Hello! <span className={styles.primaryColor}>Welcome</span> Back 🎉
+      </h3>
+      <form className={styles.form}>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Enter your password"
+          className={styles.input}
+        />
+        <div className={styles.buttonWrapper}>
+          <button type="submit" className={styles.button}>
+            Login
+          </button>
+        </div>
+        <p className={styles.signupText}>
+          Don&apos;t have an account? <Link href="/register" className={styles.signupLink}>Sign Up</Link>
+        </p>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from 'react';
+import styles from './register.module.css';
+import Link from 'next/link';
 
-const Register = () => {
+const Register: React.FC = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className={styles.container}>
+      <h3 className={styles.heading}>
+        Create your <span className={styles.primaryColor}>Account</span>
+      </h3>
+      <form className={styles.form}>
+        <input
+          type="text"
+          placeholder="Full Name"
+          className={styles.input}
+        />
+        <input
+          type="email"
+          placeholder="Enter your email"
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Enter your password"
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Confirm your password"
+          className={styles.input}
+        />
+        <div className={styles.buttonWrapper}>
+          <button type="submit" className={styles.button}>
+            Register
+          </button>
+        </div>
+        <p className={styles.signupText}>
+          Already have an account? <Link href="/login" className={styles.signupLink}>Login</Link>
+        </p>
+      </form>
+    </div>
+  );
+};
 
 export default Register;
